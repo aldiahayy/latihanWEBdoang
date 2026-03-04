8108e1ca414512e8e2e4cb54ab4cb9bfe332b823
aldi <aldisalam0756@gmail.com>
2026-03-04 17:50:32 +0800

    java script

---
+++ kelas terbuka js/part 03 - tipe data/script.js
@@ -0,0 +1,28 @@
+// tipe data pada java script
+
+let dataString = "Juki"; // string
+console.log(dataString);
+console.log(typeof dataString); // cara mengecek tipe data dari suatu data
+
+let dataNumber = 16.324; // number
+console.log(dataNumber);
+console.log(typeof dataNumber); // cara mengecek tipe data dari suatu data
+
+let dataBoolean = false; // boolean
+console.log(dataBoolean);
+console.log(typeof dataBoolean); // cara mengecek tipe data dari suatu data
+
+dataString = 2;
+console.log(dataString);
+console.log(typeof dataString); // cara mengecek tipe data dari suatu data
+
+// ini tipe data yang undefined
+let dataKosong;
+console.log(dataKosong);
+console.log(typeof dataKosong);
+dataKosong = "Ijaxtz";
+console.log(dataKosong);
+console.log(typeof dataKosong);
+
+// ini NaN
+// Math.sqrt(-1)
